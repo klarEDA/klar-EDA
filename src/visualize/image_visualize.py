33e2ba8e1d6142bcb6cb76ff83084ea13b467d71
Sayali Deshpande <35720212+psy2d@users.noreply.github.com>
2020-09-13 14:33:27 +0530

    Add t-sne visualization and update std vs mean plot

--- src/visualize/image_visualize.py
+++ src/visualize/image_visualize.py
@@ -6,13 +6,21 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from constants import VIZ_ROOT
 import cv2
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.applications.resnet50 import preprocess_input
+from tqdm import tqdm
+from sklearn.manifold import TSNE
+
 
 ############################################################################
-# To do: 1) resize images before mean / eigen images
-#        2) num components in eigen images
-#        3) optimize mean/eigen computation
-#        4) optimize std vs mean
-#        5) object detection
+# To do: 1) resizing for the funnctions that require uniform size
+#        2) handle rgb/gray images 
+#        3) axis labels, plot title  
+#        4) num components in eigen images
+#        5) optimize mean/eigen computation
+#        6) optimize std vs mean, different types of plots
+#        7) object detection - plot x,y
+#        8) batched feature extraction
 ############################################################################
 
 class ImageDataVisualize:
@@ -20,6 +28,12 @@ class ImageDataVisualize:
     def __init__(self, data, labels, boxes=None):
         self.images = data
         self.labels = labels
+        self.grey_present = False
+        for image in self.images:
+            if image.ndim < 3 or image.shape[-1] == 1:
+                self.grey_present =True
+                break
+        # self.images = [np.expand_dims(image, axis=2) if image.ndim < 3 else image for image in self.images]
         if len(self.images) != len(self.labels):
             raise ValueError('Number of images != Number of labels')
         self.validate_images()
@@ -113,5 +127,34 @@ class ImageDataVisualize:
             x.append(mean)
             y.append(std)
             hue.append(group[0])
-        plot = sns.scatterplot(x=x, y=y, hue=hue)
-        self.save_or_show(plot.figure, 'std_vs_mean', 'std_vs_mean', save=save, show=show)
+        plot = sns.scatterplot(x=x, y=y, hue=hue, palette='viridis', legend='full')
+        self.save_or_show(plot.figure, 'std_vs_mean', 'std_vs_mean_categories', save=save, show=show)
+
+        means = self.dataset['Image'].apply(np.mean).to_list()
+        stds = self.dataset['Image'].apply(np.std).to_list()
+        labels = self.dataset['Label'].to_list()
+        plot = sns.scatterplot(x=means, y=stds, hue=labels, palette='viridis', legend='full')
+        self.save_or_show(plot.figure, 'std_vs_mean', 'std_vs_mean_all', save=save, show=show)
+
+    def t_sne(self, batch_size=32, save=True, show=False):
+        model = ResNet50(weights='imagenet', pooling=max, include_top = False)
+        features_list = []
+        print('Extracting features ...')
+        for image in tqdm(self.images):
+            if self.grey_present and (image.ndim < 3 or image.shape[-1] == 1):
+                image = np.stack((image.squeeze(),)*3, axis=-1)
+            image = np.expand_dims(image, axis=0) 
+            image = preprocess_input(image) 
+            features = model.predict(image) 
+            features_reduce = features.squeeze()
+            features_list.append(features_reduce)
+
+        print('Performing t-SNE ...')
+        tsne = TSNE(n_components=2).fit_transform(features_list)
+        x = tsne[:, 0]
+        y = tsne[:, 1]
+        x = (x - np.min(x)) / np.ptp(x)
+        y = (y - np.min(y)) / np.ptp(y)
+
+        plot = sns.scatterplot(x=x, y=y, hue=self.labels, palette='viridis', legend='full')
+        self.save_or_show(plot.figure, 'tsne', 'tsne', save=save, show=show)
