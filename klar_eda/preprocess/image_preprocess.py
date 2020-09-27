85114a75495989246a77f8980f810a4728f5cb81
ask149 <ashishkshirsagar@gmail.com>
2020-09-27 19:07:36 +0530

    Refactor code

--- klar_eda/preprocess/image_preprocess.py
+++ klar_eda/preprocess/image_preprocess.py
@@ -5,7 +5,7 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-from constants import VIZ_ROOT
+from .constants import VIZ_ROOT
 
 class ImagePreprocess:
 
