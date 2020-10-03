b8f0ddaf98e0bb62b2ca9f29717003f6bff59ea3
ask149 <ashishkshirsagar@gmail.com>
2020-10-04 00:32:39 +0530

    Restructure code for publishing docs on GitHub

--- docsource/conf.py
+++ docsource/conf.py
@@ -27,7 +27,10 @@ author = 'Sayali, Rishabh, Ishaan, Ashish'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.githubpages',
+]
 
 
 # Add any paths that contain templates here, relative to this directory.
