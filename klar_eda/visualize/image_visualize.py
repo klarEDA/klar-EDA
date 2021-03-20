a8d2bf8645c3d0003a69af525941386584a59d8b
Sibasish-Padhy <70088281+Sibasish-Padhy@users.noreply.github.com>
2021-03-20 14:09:12 +0530

    Update image_visualize.py

--- klar_eda/visualize/image_visualize.py
+++ klar_eda/visualize/image_visualize.py
@@ -24,7 +24,12 @@ from sklearn.manifold import TSNE
 ############################################################################
 
 class ImageDataVisualize:
-# the first function is used intialize/get data in the form of images and labels.It also checks/looksout for the greyscale values.Also it checks whether the number of images are equal to the no of labels for naming /labelling properly.If it is not found an error is shown.
+# the first function is used intialize/get data in the form of images and labels.
+#It also checks/looksout for the greyscale values.Also it checks whether the number of images are equal to the no of labels for naming /labelling properly.
+#If it is not found an error is shown.this is used for validating the images.
+#The dataset is then prepared from the dataframe by giving all the appropriate tags to the respective dataframes.Image,height,width labels.
+#Thea area is then calculated from the height and width and the number of imagesare then printed out.
+
     def __init__(self, data, labels, boxes=None):
         self.images = data
         self.labels = labels
@@ -48,7 +53,13 @@ class ImageDataVisualize:
         })
         self.dataset['area'] = self.dataset['Height'] * self.dataset['Width']
         print('Number of images after validation and filtering:', self.num_images)
-
+#This function is written for saving the file(of the program) for better access and smooth running.
+#it looks for the  directory where the file can be saved by applying the join function if actually it is saved.
+# It looks for the directory of the file where it  can be saved (by using the join function) and if such directory doen't exist a new directory has to be made by using the makedirs function.
+# the x_label and y_label are also been given their respective titles.
+# the title of the plot is being labelled  by combining (formatting) the plot_type and file_name.
+# the path is saved by the join fucntion which combines the directory and the file name (save_dir,file_name).
+#  the title is then displayed.
     def save_or_show(self, plot, plot_type, file_name,x_label=None, y_label=None, save=True, show=False):
         if save:
             save_dir = join(VIZ_ROOT, plot_type)
@@ -65,6 +76,10 @@ class ImageDataVisualize:
             plt.title("{}: {}".format(plot_type, file_name))
             plt.show()
         plt.clf()
+# this function is used to check about the data we obtained from the images that whether or not its in the desired format or not.
+# if the image type isn't in an n dimensional array format then it is discarded else it is saved.
+#if the image type is in ndimensional array format then its accepted or it is discarded.
+#also if the dimensions of the image <=2,then again the images are skipped/discarded.
 
     def validate_images(self):
         for image, label in zip(self.images, self.labels):
@@ -78,17 +93,24 @@ class ImageDataVisualize:
                 self.images.remove(image)
                 self.labels.remove(label)
                 continue
-
+# this function is used to define the aspect_ratio of the histogram plotted.
+# the aspect ratrio of the histogram plotted is the ratio of its idth to the height(ratio=width/height)
+# It is commonly used  to describe the proportions of a rectangular screen.
     def aspect_ratio_histogram(self, save=True, show=False):
         aspect_ratios = self.dataset['Width'] / self.dataset['Height']
         plot = sns.histplot(aspect_ratios, bins='auto')
         self.save_or_show(plot.figure, 'aspect_ratios', 'aspect_ratios', x_label='aspect_ratios', y_label='frequency', save=save, show=show)
+# In this function we segregate the areas by their categories(labels) and then take the mean per category.
+#then we display those figures.
 
     def area_vs_category(self, save=True, show=False):
         mean_areas = self.dataset.groupby('Label')['area'].mean()
         plot = sns.barplot(x=mean_areas.index, y=mean_areas.tolist())
         self.save_or_show(plot.figure, 'area_vs_category', 'area_vs_category', x_label='category',y_label= 'area', save=save, show=show)
-
+#In this function we take the mean of the images segregated in groups as per their labels.
+#in these we then choose a matrixof datset and name them images.
+# we take the mean and by rows(or columns) and standardize it.
+# now we display it.
     def mean_images(self, save=True, show=False):
         groups = self.dataset.groupby('Label')
         for group in groups:
@@ -96,7 +118,10 @@ class ImageDataVisualize:
             mean_image = np.array(list(images)).mean(axis=0)
             plot = plt.imshow(mean_image/255)
             self.save_or_show(plot.figure, 'mean_images', str(group[0]), save=save, show=show)
-
+# in this function we find the eigen values and the eigen vectors of the system through principal component analysis.
+#find the mean of the eigenvectors.
+#we change the dimension of the mean matrix.
+#finding the eigen images by rounding off the eigen vectors and then displaying it.
     def eigen_images(self, save=True, show=False):
         groups = self.dataset.groupby('Label')
         for group in groups:
